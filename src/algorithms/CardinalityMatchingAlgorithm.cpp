43ab511f3095582f5e1bbc7d30b7d00e7fcdcba5
bnw <s6beklot@uni-bonn.de>
2016-12-03 19:02:12 +0100

    Refactoring

---
+++ src/algorithms/CardinalityMatchingAlgorithm.cpp
@@ -0,0 +1,113 @@
+#include "CardinalityMatchingAlgorithm.h"
+
+namespace algorithms {
+
+Matching algorithms::CardinalityMatchingAlgorithm::run(ShrinkableGraph &G) const
+{
+	Matching M(G);
+	return run(G, M);
+}
+
+Matching algorithms::CardinalityMatchingAlgorithm::run(ShrinkableGraph &G, Matching &M) const
+{
+	while (M.has_exposed_vertex()) {
+		Edge::Vector L;
+		auto T = reset_alternating_tree(M, G, L);
+		while (not L.empty()) {
+			auto const edge = pop(L);
+			if (G.is_contracted_edge(edge) or T->contains_edge(edge)) {
+				continue;
+			}
+			auto even_vertices = get_even_edge_vertices(edge, *T);
+			if (even_vertices.size() == 2) {
+				auto const C = create_circuit(edge, *T, G);
+				shrink_circuit(C, G, *T, M, L);
+			} else if (even_vertices.size() == 1) {
+				auto const even_vertex = even_vertices.at(0);
+				auto const other_vertex = edge.other_vertex(even_vertex);
+				if (T->is_odd(other_vertex)) {
+					continue;
+				}
+				if (M.is_exposed(other_vertex)) {
+					auto augmenting_path = T->find_path_from_root_to_node(even_vertex);
+					augmenting_path.push_back(edge);
+					M.augment(augmenting_path);
+					unshrink_matching(M, G);
+					if (M.is_perfect()) {
+						return M;
+					}
+					G.reset();
+					T = reset_alternating_tree(M, G, L);
+				} else {
+					auto const covering_edge = M.get_covering_edge(other_vertex);
+					auto const z = covering_edge.other_vertex(other_vertex);
+					T->add_edge(edge);
+					T->add_edge(covering_edge);
+					append(L, G.get_incident_edges(z));
+				}
+			}
+		}
+		unshrink_matching(M, G);
+		G.reset();
+		G.deactivate_nodes(T->get_nodes());
+
+//			std::cout << "|M| = " << M.get_num_edges()
+//					  << ", G.num_active_nodes = " << G.get_num_active_nodes() << std::endl;
+	}
+	return M;
+}
+
+void algorithms::CardinalityMatchingAlgorithm::unshrink_matching(Matching &M, ShrinkableGraph const &G) const
+{
+	MatchingUnshrinkAlgorithm(M, G);
+}
+
+void
+algorithms::CardinalityMatchingAlgorithm::shrink_circuit(OddCircuit const &C, ShrinkableGraph &G, AlternatingTree &T,
+		Matching &M, Edge::Vector &L) const
+{
+	for (auto const node_id : C.get_node_ids()) {
+		assert(T.contains_node(node_id));
+		if (T.is_odd(node_id)) {
+			append(L, G.get_incident_edges(node_id));
+		}
+	}
+	G.shrink_circuit(C);
+	T.shrink_circuit(C);
+	M.remove_edges_if_contained(C.get_edges());
+}
+
+OddCircuit algorithms::CardinalityMatchingAlgorithm::create_circuit(Edge const &edge, AlternatingTree const &T,
+		ShrinkableGraph const &G) const
+{
+	auto circuit_edges = T.find_path(edge.first_node_id(), edge.second_node_id());
+	circuit_edges.push_back({edge.second_node_id(), edge.first_node_id()});
+	return OddCircuit(G, circuit_edges);
+}
+
+Edge algorithms::CardinalityMatchingAlgorithm::pop(Edge::Vector &L) const
+{
+	auto const edge = L.back();
+	L.pop_back();
+	return edge;
+}
+
+std::vector<unsigned long, std::allocator<unsigned long>>
+algorithms::CardinalityMatchingAlgorithm::get_even_edge_vertices(Edge const &edge, AlternatingTree const &T) const
+{
+	return filter<NodeId>(
+			edge.get_node_ids(),
+			[&](NodeId n) { return T.is_even(n); }
+	);
+}
+
+std::unique_ptr <AlternatingTree, std::default_delete<AlternatingTree>>
+algorithms::CardinalityMatchingAlgorithm::reset_alternating_tree(Matching const &M, ShrinkableGraph const &G,
+		Edge::Vector &L) const
+{
+	NodeId const r = M.get_one_exposed();
+	L = G.get_incident_edges(r);
+	return std::unique_ptr<AlternatingTree>(new AlternatingTree(G, r));
+}
+}
+
--- src/algorithms/CardinalityMatchingAlgorithm.h
+++ src/algorithms/CardinalityMatchingAlgorithm.h
@@ -11,79 +11,42 @@ namespace algorithms {
 class CardinalityMatchingAlgorithm
 {
 public:
-	Matching run(ShrinkableGraph &G) const
-	{
-		Matching M(G);
-		while (M.has_exposed_vertex()) {
-			NodeId const r = M.get_one_exposed();
-			std::unique_ptr<AlternatingTree> T(new AlternatingTree(G, r));
-			auto L = G.get_incident_edges(r);
-			while (not L.empty()) {
-				auto const &T_debug = *T;
-				auto const edge = L.back();
-				L.pop_back();
-				if (G.is_contracted_edge(edge) or T->contains_edge(edge)) {
-					continue;
-				}
-				auto even_vertices = filter<NodeId>(
-						edge.get_node_ids(),
-						[&](NodeId n) { return T->is_even(n); }
-				);
-				if (even_vertices.size() == 2) {
-					auto circuit_edges = T->find_path(even_vertices.at(0), even_vertices.at(1));
-					circuit_edges.push_back({even_vertices.at(1), even_vertices.at(0)});
-					//TODO
-					OddCircuit C(G, circuit_edges);
-					for (auto const node_id : C.get_node_ids()) {
-						assert(T->contains_node(node_id));
-						if (T->is_odd(node_id)) {
-							append(L, G.get_incident_edges(node_id));
-						}
-					}
-					G.shrink_circuit(C);
-					T->shrink_circuit(C);
-					M.remove_edges_if_contained(circuit_edges);
-				} else if (even_vertices.size() == 1) {
-					auto const even_vertex = even_vertices.at(0);
-					auto const other_vertex = edge.other_vertex(even_vertex);
-					if (T->is_odd(other_vertex)) {
-						continue;
-					}
-					if (M.is_exposed(other_vertex)) {
-						auto augmenting_path = T->find_path_from_root_to_node(even_vertex);
-						augmenting_path.push_back(edge);
-						M.augment(augmenting_path);
-						MatchingUnshrinkAlgorithm(M, G);
-						if (M.is_perfect()) {
-							std::cout << "Found perfect matching with " << M.get_num_edges() << " edges!"
-									  << std::endl;
-							return M;
-						}
-						G.reset();
-						auto const new_root = M.get_one_exposed();
-						T = std::unique_ptr<AlternatingTree>{new AlternatingTree(G, new_root)};
-						L = G.get_incident_edges(new_root);
-					} else {
-						assert(M.is_covered(other_vertex));
-						assert(not T->contains_node(other_vertex));
-						T->add_edge(edge);
-						auto const covering_edge = M.get_covering_edge(other_vertex);
-						auto const z = covering_edge.other_vertex(other_vertex);
-						T->add_edge(covering_edge);
-						append(L, G.get_incident_edges(z));
-					}
-				}
-			}
-			//TODO remove debug
-			MatchingUnshrinkAlgorithm(M, G);
-			G.reset();
-			G.deactivate_nodes(T->get_nodes());
 
-//			std::cout << "|M| = " << M.get_num_edges()
-//					  << ", G.num_active_nodes = " << G.get_num_active_nodes() << std::endl;
-		}
-		return M;
-	}
+	/**
+	 * Compute a maximum matching.
+	 */
+	Matching run(ShrinkableGraph &G) const;
+
+	/**
+	 * Improve a given matching to maximum cardinality.
+	 * @param G graph
+	 * @param M hint matching
+	 */
+	Matching run(ShrinkableGraph &G, Matching &M) const;
+
+private:
+	void unshrink_matching(Matching &M, ShrinkableGraph const &G) const;
+
+	void shrink_circuit(
+			OddCircuit const &C,
+			ShrinkableGraph &G,
+			AlternatingTree &T,
+			Matching &M,
+			Edge::Vector &L
+	) const;
+
+	OddCircuit create_circuit(Edge const &edge, AlternatingTree const &T, ShrinkableGraph const &G) const;
+
+	Edge pop(Edge::Vector &L) const;
+
+	std::vector<NodeId> get_even_edge_vertices(Edge const &edge, AlternatingTree const &T) const;
+
+	std::unique_ptr<AlternatingTree> reset_alternating_tree(
+			Matching const &M,
+			ShrinkableGraph const &G,
+			Edge::Vector &L
+	) const;
+
 };
 
 }
